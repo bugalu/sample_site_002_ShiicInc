3d2001cb75732c1e3b4a9027d29417f5a9c26a0f
ikeda-shin <ikeda-shin@office-chico.biz>
2020-09-15 11:22:45 +0900

    画像ファイル名を整理

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,11 +1,3 @@
-"use strict";
-{
-  const show = document.getElementById("show");
-  const globalNav = document.getElementById("globalNav");
-  const shopNav = document.getElementById("shopNav");
 
-  show.addEventListener("click", function() {
-    globalNav.classList.toggle("menu-open");
-    shopNav.classList.toggle("menu-open");
-  });
-}
+
+//# sourceMappingURL=main.js.map
