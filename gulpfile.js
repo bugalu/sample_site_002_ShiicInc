820c6dc6b12d75c770466cd54fa122a4506e891c
bugalu <ikedashin1101@gmail.com>
2021-01-06 16:20:25 +0900

    h1ロゴのパスを修正

--- gulpfile.js
+++ gulpfile.js
@@ -1,53 +1,51 @@
-const { src, dest, watch, series, parallel } = require('gulp');
-const loadPlugins = require('gulp-load-plugins');
+const { src, dest, watch, series, parallel } = require("gulp");
+const loadPlugins = require("gulp-load-plugins");
 const $ = loadPlugins();
 
 //ベンダープレフィックス
-const autoprefixer = require('autoprefixer');
+const autoprefixer = require("autoprefixer");
 
 //動作確認用サーバー
-const browserSync = require('browser-sync');
+const browserSync = require("browser-sync");
 const server = browserSync.create();
 
 function compile() {
-  return src(['./src/ejs/**/*.ejs', '!' + './src/ejs/**/_*.ejs'])
+  return src(["./src/ejs/**/*.ejs", "!" + "./src/ejs/**/_*.ejs"])
     .pipe($.ejs({}, {}, { ext: ".html" }))
     .pipe($.rename({ extname: ".html" }))
-    .pipe(dest('./dist'));
+    .pipe(dest("./dist"));
 }
 
 function styles() {
-  return src('./src/sass/**/*.scss')
+  return src("./src/sass/**/*.scss")
     .pipe($.sourcemaps.init())
     .pipe($.sass())
-    .pipe($.postcss([
-      autoprefixer()
-    ]))
-    .pipe($.csscomb('zen.json'))
-    .pipe($.sourcemaps.write('.'))
-    .pipe(dest('./dist/css'));
+    .pipe($.postcss([autoprefixer()]))
+    .pipe($.csscomb("zen.json"))
+    .pipe($.sourcemaps.write("."))
+    .pipe(dest("./dist/css"));
 }
 
 function scripts() {
-  return src('./src/js/*.js')
+  return src("./src/js/*.js")
     .pipe($.sourcemaps.init())
     .pipe($.babel())
-    .pipe($.sourcemaps.write('.'))
-    .pipe(dest('./dist/js'));
+    .pipe($.sourcemaps.write("."))
+    .pipe(dest("./dist/js"));
 }
 
 function startAppServer() {
   server.init({
     server: {
-      baseDir: './dist'
-    }
+      baseDir: "./dist",
+    },
   });
-  watch('./src/**/*.ejs', compile);
-  watch('./src/**/*.ejs').on('change', server.reload);
-  watch('./src/**/*.scss', styles);
-  watch('./src/**/*.scss').on('change', server.reload);
-  watch('./src/**/*.js', scripts);
-  watch('./src/**/*.js').on('change', server.reload);
+  watch("./src/**/*.ejs", compile);
+  watch("./src/**/*.ejs").on("change", server.reload);
+  watch("./src/**/*.scss", styles);
+  watch("./src/**/*.scss").on("change", server.reload);
+  watch("./src/**/*.js", scripts);
+  watch("./src/**/*.js").on("change", server.reload);
 }
 
 const serve = series(parallel(compile, styles, scripts), startAppServer);
